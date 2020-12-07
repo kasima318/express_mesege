eaca9f9bf472d7f29dbd1bb50fbdf92f02405994
kasima318 <190157ol@yse-c.net>
2020-12-07 11:41:58 +0900

    sever masege input

--- server.js
+++ server.js
@@ -1,13 +1,38 @@
 const express =require('express');
 
+
 const app = express();
 
+// URLエンコードされたデータを解析する
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.listen(3000);
+
+//CROS設定: 全てのドメインに対して許可 
+app.use((req, res, next) => {
+    console.log(`middleware: all. url: ${req.url}`);
+
+    //CROS設定: 全てのドメインに対して許可 
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+    //次の処理
+    next();
+});
 
 //ルーティング
 //webルートにリクエストされたらレスポンス
-app.get('/',(req,res) =>{
-    res.send("hello Express!");
+// app.get('/',(req,res) =>{
+//     res.send("hello Express!");
+// });
+
+app.post('/',(req,res) =>{
+
+    let result = {
+        message: req.body.message,
+        datetime: new Date(),
+    };
+    res.send(result);
 });
 
 //app.get(URLパス,処理);
