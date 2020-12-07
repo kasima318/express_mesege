e70c8dd160509a163848a557b023442300473581
kasima318 <190157ol@yse-c.net>
2020-12-07 12:10:09 +0900

    /purchase

--- server.js
+++ server.js
@@ -35,9 +35,31 @@ app.post('/',(req,res) =>{
     res.send(result);
 });
 
+app.post('/purchase', (req, res) => {
+    const tax_rate = 0.1;
+    const price = req.body.price;
+    const amount = req.body.amount;
+
+    const tax = price * amount * tax_rate;
+    const excluding_price = price * amount;
+    const total_price = excluding_price + tax;
+
+    let result = {
+        excluding_price: excluding_price,
+        total_price: total_price,
+        tax: tax,
+    };
+        //レスポンス
+        res.send(result);
+    });
+
+
 //app.get(URLパス,処理);
 
 //無名関数の書き方
 //app.get('/',function(req,res){
 
 //});
+
+app.use(express.static(__dirname + '/public'));
+
